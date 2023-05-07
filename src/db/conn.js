86ca339232c04670012b7fff4113b9544f4651da
sanketkittad <sanketkittad02@gmail.com>
2023-05-07 23:38:26 +0530

    added styles

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/youtubeRegistration", {
+mongoose.connect("mongodb://127.0.0.1:27017/youtubeRegistration", {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
